f9d704c3719d6b78f17ca1524874711f0aefed6f
Stefan van den Dungen Gronovius <stefan@codepress.nl>
2017-11-06 13:37:44 +0100

    Array check

--- classes/Column/Experimental.php
+++ classes/Column/Experimental.php
@@ -7,7 +7,7 @@ class ACA_Extra_Columns_Column_Experimental extends AC_Column {
 	}
 
 	public function is_valid() {
-		return in_array( $this->get_type(), ac_addon_extra_columns()->get_active_extra_columns() );
+		return in_array( $this->get_type(), (array) ac_addon_extra_columns()->get_active_extra_columns() );
 	}
 
 }
